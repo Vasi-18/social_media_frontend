23a80855f414f7462544373ccf6f13dae6e83f5d
Vasi-18 <vasanthkumar1822000@gmail.com>
2023-09-12 21:28:03 +0530

    social media app

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,9 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+
+export const Followers = [
+  { name: "Bala", username: "bala_krishnan", img: img1 },
+  { name: "Suhail", username: "suhail_basha", img: img2 },
+  { name: "Hamnath", username: "hamanth_kbh", img: img3 },
+];
